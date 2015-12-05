2e8c034bcdd769c9ce55cc5d3b0055b470fc7c52
Alexander Garcia <alexrymon@gmail.com>
2015-12-04 21:49:10 -0500

    Changed to alpha values

--- js/waldo.js
+++ js/waldo.js
@@ -1,7 +1,6 @@
 var count = 0;
-var topx;       //for clicking function
-var topy;       //for clicking function
-var bottom;     //for clicking function
+var top;
+var bottom;
 var image = null;          // The image that the user interacts with
 var modified_image = null;
 var ctx;            // canvas context
@@ -21,17 +20,14 @@ window.onload = function() {
 	ctx = canvas.getContext('2d');
     canvas.addEventListener('click', function(event){
         count++
-        if(count%2 == 0){
-            bottom = [event.pageX-canvas.offsetLeft,event.pageY-canvas.offsetTop];
-            console.log(bottom);
-            crop([topx, topy],bottom);
-            count = 0
-        }
-        else{
-            topx = event.pageX-canvas.offsetLeft;
-            topy = event.pageY-canvas.offsetTop;
-            console.log(topx, topy);
-        }
+  	if(count == 2){
+    		bottom = [event.pageX-cavas.offsetLeft,event.pageY-canvas.offsetTop];
+    		canvas(top,bottom);
+    		count = 0
+  	}
+  	else{
+    		top = [event.pageX-cavas.offsetLeft,event.pageY-canvas.offsetTop];
+  	}
     });
     //console.log(image.height);
     //console.log(image.width);
@@ -165,9 +161,10 @@ function crop(top,bottom) {
     console.log(imageData);
     var data = imageData.data;
     for (var i = 0; i < data.length; i += 4) {
-        data[i]     = data[i]-105;     // red
-        data[i + 1] = data[i+1]-105; // green
-        data[i + 2] = data[i+2]-105; // blue
+        data[i]     = 255;     // red
+        data[i + 1] = 255; // green
+        data[i + 2] = 255; // blue
+        data[i + 3] = 150;
     }
     ctx.putImageData(imageData, 0, 0, 0, 0, top[0], modified_image.height);
     ctx.putImageData(imageData, 0, 0, 0, 0, modified_image.width, top[1]);
@@ -175,9 +172,10 @@ function crop(top,bottom) {
     console.log(imageData);
     var data = imageData.data;
     for (var i = 0; i < data.length; i += 4) {
-        data[i]     = data[i] - 105;     // red
-        data[i + 1] = data[i+1] - 105; // green
-        data[i + 2] = data[i+2] - 105; // blue
+        data[i]     = 255;     // red
+        data[i + 1] = 255; // green
+        data[i + 2] = 255; // blue
+        data[i + 3] = 150;
     }
     ctx.putImageData(imageData, top[0],bottom[1], 0, 0, modified_image.width, modified_image.height);
     
@@ -185,9 +183,10 @@ function crop(top,bottom) {
     console.log(imageData);
     var data = imageData.data;
     for (var i = 0; i < data.length; i += 4) {
-        data[i]     = data[i] - 105;     // red
-        data[i + 1] = data[i+1] - 105; // green
-        data[i + 2] = data[i+2] - 105; // blue
+        data[i]     = 255;     // red
+        data[i + 1] = 255; // green
+        data[i + 2] = 255; // blue
+        data[i + 3] = 150;
     }
     ctx.putImageData(imageData, bottom[0], top[1], 0, 0, modified_image.width, bottom[1]-top[1]);
 }
