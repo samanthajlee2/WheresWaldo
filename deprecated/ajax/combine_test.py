24bb032433f5c4769c652ca5cd0b1cdf16115443
Droid5x <mrkblnc1248@gmail.com>
2015-12-06 15:42:15 -0500

    Restructured directory tree for use with flask python server.

---
+++ deprecated/ajax/combine_test.py
@@ -0,0 +1,12 @@
+import cgi
+print """Content-type: text/html; charset=utf-8\n\n
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>"""
+field= cgi.FieldStorage()
+attr = field.getvalue("mymode", "error")
+
+def foo():
+	print "phello"
+	return "hello"
+
+print """</html>"""
